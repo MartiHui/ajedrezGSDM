116d1195004869793e0b32d0347e85f63f1d96b2
martihui <martiyanghui@gmail.com>
2018-03-09 12:50:53 +0100

    Clase Movimiento

--- src/ajedrez/Movimiento.java
+++ src/ajedrez/Movimiento.java
@@ -1,5 +1,30 @@
 package ajedrez;
 
 public class Movimiento {
+	public Coordenadas cOrigen, cDestino;
+	public Piezas pOrigen, pDestino;
+	
+	public Movimiento(Coordenadas cOrigen, Coordenadas cDestino, Piezas pOrigen, Piezas pDestino) {
+		this.cOrigen = cOrigen;
+		this.cDestino = cDestino;
+		this.pOrigen = pOrigen;
+		this.pDestino = pDestino;
+	}
+	
+	public boolean isCastling() {
+		return (Math.abs(this.cOrigen.distanceCasilla(cDestino).coorX) == 2);
+	}
+	
+	public boolean isPawnDoubleMove() {
+		return (Math.abs(this.cOrigen.distanceCasilla(cDestino).coorY) == 2);
+	}
+	
+	public boolean isPawnPromotion() {
+		return (this.cDestino.coorY == 0 || this.cDestino.coorY == 7);
+	}
+	
+	public boolean isEnPassant() {
+		return (this.pDestino.posicion != this.cDestino);
+	}
 
 }
